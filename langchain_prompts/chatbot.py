344fdce9c9f955868e1ae9abddad15dbc1dfa0cf
Mohd Anas Khan <119879801+anaskhaann@users.noreply.github.com>
2025-08-24 09:08:50 +0530

    feat: added basic chat history to chatbot

--- langchain_prompts/chatbot.py
+++ langchain_prompts/chatbot.py
@@ -5,14 +5,23 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
+# to store the context
+chat_history = []
+
 """we will need a infinite loop that will not exit the chat until user press the exit"""
 
 while True:
     user_query = input("You: ")
+    chat_history.append(user_query)
 
-    # if user type exit then break
     if user_query == "exit":
         break
-    result = model.invoke(user_query)
+    result = model.invoke(chat_history)
+
+    # append the result to the chat_history also
+    chat_history.append(result.content)
 
     print(f"AI: {result.content}")
+
+
+print(chat_history)
