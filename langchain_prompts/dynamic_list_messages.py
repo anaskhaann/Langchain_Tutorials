9588e211388847e367da8b16e9d0c79760b2ba9a
Mohd Anas Khan <119879801+anaskhaann@users.noreply.github.com>
2025-08-25 11:56:01 +0530

    fix: update the correct code to use chat prompt template for handling inputs

--- langchain_prompts/dynamic_list_messages.py
+++ langchain_prompts/dynamic_list_messages.py
@@ -1,18 +1,18 @@
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
+
+"""
+The problem was we were getting place holder as it is.
+It is not replacing dictionary values.
+--> TO fix this problem we will pass heirarchical structure in tuple rather than using system messages.
+"""
 
 chat_template = ChatPromptTemplate(
     [
-        SystemMessage(content="You are a helpful {domain} expert"),
-        HumanMessage(content="Explain in simple terms, what is {topic}"),
+        ("system", "You are a helpful {domain} expert"),
+        ("human", "Explain in simple terms, what is {topic}"),
     ]
 )
 
 prompt = chat_template.invoke({"domain": "cricket", "topic": "Dusra"})
-
-"""
-The problem is we are getting place holder as it is.
-It is not replacing dictionary values.
-"""
 
 print(prompt)
