e0a9d89a9ded8c31fde10ce8e520dc8104eea47c
Mohd Anas Khan <119879801+anaskhaann@users.noreply.github.com>
2025-08-25 12:57:52 +0530

    feat: add a chat history in dynamic list of messages using Message PlaceHolder

--- langchain_prompts/dynamic_list_messages.py
+++ langchain_prompts/dynamic_list_messages.py
@@ -1,18 +1,26 @@
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-"""
-The problem was we were getting place holder as it is.
-It is not replacing dictionary values.
---> TO fix this problem we will pass heirarchical structure in tuple rather than using system messages.
-"""
-
+# create chat template
 chat_template = ChatPromptTemplate(
     [
-        ("system", "You are a helpful {domain} expert"),
-        ("human", "Explain in simple terms, what is {topic}"),
-    ]
+        ("system", "You are a helpful customer support agent"),
+        # now this query will not be understood by our customer support because it has some history related to current query so insert a message place holder
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{query}"),
+    ],
 )
 
-prompt = chat_template.invoke({"domain": "cricket", "topic": "Dusra"})
+chat_history = []
+
+# load history
+with open("message_placeholder_history.txt") as f:
+    chat_history.extend(f.readlines())
+
+print(chat_history)
+
+# prompt from template: this will replace variables to values
+prompt = chat_template.invoke(
+    {"chat_history": chat_history, "query": "Where is my refund?"}
+)
 
 print(prompt)
